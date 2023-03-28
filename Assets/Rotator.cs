9fa0df991211bbdbbff9ae589c50920a13867a9d
adiy55 <adiyafe55@gmail.com>
2023-03-28 13:17:56 +0300

    Add donut + use with rotator script

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -6,7 +6,7 @@ public class Rotator : MonoBehaviour
 {
     [SerializeField] float speed = 100.0f;
     
-    Vector3 startPos = new Vector3(0,0,0);
+    Vector3 startPos = new Vector3(0, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
