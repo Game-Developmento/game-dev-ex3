883732a8dcca61044c4bf872d59c2b9d864f0d6d
adiy55 <adiyafe55@gmail.com>
2023-03-28 12:49:00 +0300

    Remove redundant files + add oscillator values per object

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -2,35 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// useful source: https://youtu.be/fwSWZ-DNm7Y
+
 public class Oscillator : MonoBehaviour
 {
     
-    [SerializeField] Vector3 movement = new Vector3(0,2,1); // The movement vector that determines the direction and magnitude of movement
-    float movementFactor; // A scalar value between 0 and 1 that determines the position along the movement vector
+    [SerializeField] Vector3 movement; // movement vector - determines the direction and magnitude of movement
+    float movementFactor; // value between 0 and 1 - determines the position along the movement vector
 
-    [SerializeField] float period = 4f; // The time in seconds it takes to complete one full oscillation cycle
-    Vector3 startPos; // The starting position of the object
+    [SerializeField] float period; // time (in seconds) to complete one oscillation cycle
+    Vector3 startPos; // store position - we want the object to move relative to its starting position
     
-    // Start is called before the first frame update
     void Start()
     {
-        // We want the object to move relative to its starting position
-        startPos = transform.position; // Store the starting position of the object
+        switch(this.name) {
+            case "Sphere":
+                movement = new Vector3(2, 0, 0);
+                period = 1f;
+                break;
+            case "Cube":
+                movement = new Vector3(0, 1, 2);
+                period = 5f;
+                break;
+            case "Cylinder":
+                movement = new Vector3(0, 0, 3);
+                period = 10f;
+                break;
+            default:
+                movement = new Vector3(0, 2, 1);
+                period = 4f;
+                break;
+        }
+        startPos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (period <= 0f) { return; } // Exit early if the period is invalid
+        if (period <= 0f) { return; }
         
-        // Calculate the number of cycles completed since the game started
-        float cycles = Time.time / period;
+        float cycles = Time.time / period; // number of cycles completed since the game started
 
-        const float tau = Mathf.PI * 2; // Tau (τ) is equal to 2π radians
-        float movementFactor = Mathf.Sin(cycles * tau); // Calculate the raw sine wave value based on the number of cycles completed
-        Vector3 offset = movement * movementFactor; // Calculate the offset to apply to the starting position
-        Debug.Log(offset);
+        const float tau = Mathf.PI * 2;
+        float movementFactor = Mathf.Sin(cycles * tau); // sine wave value based on the number of cycles completed
+        Vector3 offset = movement * movementFactor;
+        // Debug.Log(offset);
 
-        transform.position = startPos + offset; // Move the object to its new position
+        transform.position = startPos + offset; // move the object to its new position
     }
 }
