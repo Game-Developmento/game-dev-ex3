2db50b59280554ddfd274ae8f98a2d382676640c
OriHoward <ori.howard@gmail.com>
2023-03-28 19:14:58 +0300

    Added new assests, started creating new 2d game

---
+++ Assets/Mover.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 6f;
+
+    [SerializeField]
+    InputAction moveUp = new InputAction(type: InputActionType.Button);
+
+    [SerializeField]
+    InputAction moveDown = new InputAction(type: InputActionType.Button);
+
+    [SerializeField]
+    InputAction moveLeft = new InputAction(type: InputActionType.Button);
+
+    [SerializeField]
+    InputAction moveRight = new InputAction(type: InputActionType.Button);
+
+    void OnEnable() {
+        moveUp.Enable();
+        moveDown.Enable();
+        moveLeft.Enable();
+        moveRight.Enable();
+    }
+    void OnDisable() {
+        moveUp.Disable();
+        moveDown.Disable();
+        moveLeft.Disable();
+        moveRight.Disable();
+    }
+
+    void start() {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveUp.IsPressed()) {
+            transform.position += new Vector3(0,speed * Time.deltaTime,0);
+        }
+        else if (moveDown.IsPressed()) {
+            transform.position += new Vector3(0,-speed * Time.deltaTime,0);
+        }
+        else if (moveRight.IsPressed()) {
+            transform.position += new Vector3(speed * Time.deltaTime,0,0);
+        }
+        else if (moveLeft.IsPressed()) {
+            transform.position += new Vector3(-speed * Time.deltaTime,0,0);
+        }
+    }
+}
