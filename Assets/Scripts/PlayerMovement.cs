bbaa8b82373f7c2309d92f4041a409b4506ff15f
adiy55 <adiyafe55@gmail.com>
2023-03-28 22:44:24 +0300

    Add second player to box game + split screen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public Rigidbody rb;
 
     public float forwardForce = 1000f;
     public float sidewaysForce = 100f;
+    string leftKey, rightKey;
+
+    void Start() {
+        if(this.tag == "Player") {
+            leftKey = "a";
+            rightKey = "d";
+        } else { // Player2
+            leftKey = "left";
+            rightKey = "right";
+        }
+    }
 
     // Update is called once per frame
     void FixedUpdate() // use FixedUpdate when using physics (adding forces / changing velocity) - will look smoother
     {
         rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Time.deltaTime = amount of seconds since the computer drew the last frame (even out frame rate)
 
-        if(Input.GetKey("d")) {
+        if(Input.GetKey(rightKey)) {
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange); // velocity ignores the object mass
         }
 
-        if(Input.GetKey("a")) {
+        if(Input.GetKey(leftKey)) {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
